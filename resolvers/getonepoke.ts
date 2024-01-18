b3539b33585423a8925c14c1fccc17a2f523acf8
Ignacio García <ijgarciaest@gmail.com>
2024-01-18 12:16:29 +0100

    modified:   resolvers/getonepoke.ts

--- resolvers/getonepoke.ts
+++ resolvers/getonepoke.ts
@@ -10,6 +10,18 @@ export const getapoke = async (req: Request, res: Response) => {
       const URL="https://pokeapi.co/api/v2/pokemon/";
       const id=req.params.id;
       console.log(id);
+      const alreadyExists = await ModeloPoke.findOne({ id }).exec();
+      if (alreadyExists) {
+        res.status(400).send("Pokemon already exists");
+        const datos= await fetch(`${URL}${id}`);
+        const pokemonpelao:pokemonfinall= await datos.json();
+        res.status(200).send({
+          name:pokemonpelao.name,
+          tipo:pokemonpelao.types[0].type.name,
+          id:pokemonpelao.id
+        });
+        return;
+      }else{
       const datos= await fetch(`${URL}${id}`);
       const pokemonpelao:pokemonfinall= await datos.json();
       const newPoke= new ModeloPoke({name:pokemonpelao.name ,id:pokemonpelao.id,type:pokemonpelao.types[0].type.name})
@@ -19,6 +31,7 @@ export const getapoke = async (req: Request, res: Response) => {
         tipo:pokemonpelao.types[0].type.name,
         id:pokemonpelao.id
       });
+    }
     } catch(error) {
       res.status(500).send(error.message);
       return;
